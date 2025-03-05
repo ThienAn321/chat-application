c629d79a596b4e8bb8040347adbb3bc55b20dea4
Trinh Huu Thien An <thienan8928@gmail.com>
2025-03-05 18:26:13 +0700

    Feature: Add docker and fix front end

--- front-end-chat/src/components/chatContent/Chat.tsx
+++ front-end-chat/src/components/chatContent/Chat.tsx
@@ -1,18 +1,21 @@
 import UserInfoChat from '../common/UserInfoChat';
 import MessageInput from '../common/MessageInput';
 import MessageArea from '../common/MessageArea';
+import { useState } from 'react';
 
 const Chat = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [progress, setProgress] = useState<number>(0);
   return (
     <div className="chat">
       <div className="top">
         <UserInfoChat />
       </div>
       <div className="center">
-        <MessageArea />
+        <MessageArea loading={loading} progress={progress} />
       </div>
       <div className="bottom">
-        <MessageInput />
+        <MessageInput setLoading={setLoading} setProgress={setProgress} />
       </div>
     </div>
   );
